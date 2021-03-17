704068f61118e7a9948d2c99b73c522790aee305
k0502s <k0502s@naver.com>
2021-03-17 17:37:46 +0900

    랜딩 페이지(홈) 및 라우터 기본 세팅 완료

---
+++ client/src/components/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Row, Col } from 'reactstrap';
+
+const LandingPage = () => {
+    return (
+        <div>
+        
+             홈
+    
+        </div>
+    )
+}
+
+export default LandingPage
---
+++ client/src/components/Myrouter/Router.js
@@ -0,0 +1,36 @@
+import React, { Fragment } from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import AppNavbar from '../Nav/AppNavbar';
+import SideNav from '../Nav/SideNav';
+import Profile from '../Profile/Profile';
+import LandingPage from '../LandingPage/LandingPage';
+import { ProfileProtectedRoute } from '../../productedRoute/ProtectedRoute';
+import { Container, Row, Col } from 'reactstrap';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+const MyRouter = () => {
+    return (
+        <Fragment>
+            <Header />
+            <AppNavbar />
+            <Row>
+                <Col md={{ size: 3, offset: 1 }} xs={{ size: 12 }} sm={{ size: 10, offset: 1 }}>
+                    <SideNav />
+                </Col>
+                <Col mb={7} className='mt-3'>
+                    <Container id="main-body">
+                        <Switch>
+                            <Route exact path="/" component={LandingPage} />
+                            <ProfileProtectedRoute exact path="/user/:userName/profile" component={Profile} />
+                            <Redirect from="*" to="/" />
+                        </Switch>
+                    </Container>
+                </Col>
+            </Row>
+            <Footer />
+        </Fragment>
+    );
+};
+
+export default MyRouter;
