70e3486d7e93e2dd306cdff3e26bc48d9135e830
k0502s <k0502s@naver.com>
2021-03-24 16:46:01 +0900

    포토 갤러리 생성 페이지 파일 드랍 및 기타 CSS 수정

--- client/src/components/AddPhotoPage/AddPhotoPage.js
+++ client/src/components/AddPhotoPage/AddPhotoPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Form, Input, Label, Card, CardTitle, CardText, Row, Col } from 'reactstrap';
+import { Button, Form, Input, Label, Card, CardTitle, CardText, Row, Col, CardHeader, CardBody } from 'reactstrap';
 import FileUpload from '../utils/Fileupload';
 import { useDispatch, useSelector } from 'react-redux';
 import { PHOTO_UPLOADING_REQUEST } from '../../redux/types';
@@ -33,7 +33,7 @@ const AddPhotoPage = (props) => {
     };
 
     const updateImages = (newImages) => {
-        setImages(newImages);
+        setImages([...Images, newImages]);
     };
 
     const submitHandler = (e) => {
@@ -58,9 +58,9 @@ const AddPhotoPage = (props) => {
             return alert('사진을 입력해야 합니다.');
         }
 
-        //서버에 채운 값들을 request을 보낸다.
+       
         const body = {
-            writer: user._id, //로그인된 사람의 ID
+            writer: user._id,
             title: Title,
             description: Description,
             images: Images,
@@ -72,34 +72,53 @@ const AddPhotoPage = (props) => {
         });
     };
 
+    const removefile = () => {
+        setImages([]);
+    };
+
     return (
-        <Card body style={{height:'700px', borderRadius:'50px'}}>
-                <CardTitle tag="h3">
-                    포토 추가하기
-                </CardTitle>
-                <Form onSubmit={submitHandler} name="myform">
-                    {/* DropZone   */}
-                    <FileUpload refreshFunction={updateImages} />
-                    <Label>제목</Label>
-                    <Input onChange={titleChangeHandler} value={Title} name="title" />
-                    <br />
-                    <Label>설명</Label>
-                    <Input onChange={descriptionChangeHandler} value={Description} name="description" />
-                    <br />
-                    <Label>장르</Label>
-                    <Input onChange={continentChangeHandler} value={Continent} name="continent" type="select">
-                        <option value="">장르를 선택해주세요</option>
-                        {Continents.map((item) => (
-                            <option key={item.key} value={item.key}>
-                                {item.value}
-                            </option>
+        <Col>
+            <Card style={{ borderRadius: '25px', marginBottom: '20px' }}>
+                <CardHeader>
+                    <strong>포토 추가하기</strong>
+                </CardHeader>
+                <Card body>
+                    <CardTitle tag="h5">※ 이미지 파일 업로드 참고 사항</CardTitle>
+                    <CardText>1. 이미지 파일 사이즈는 100 * 1024 * 1024의 제한을 두고 있습니다.</CardText>
+                    <CardText>2. 추가로 한 장 이상의 이미지 파일을 업로드 할 수 있습니다.</CardText>
+                    <CardText>
+                        <FileUpload refreshFunction={updateImages} removefile={removefile}/>
+                    </CardText>
+                </Card>
+                <CardBody>
+                    <Col md={{ offset: 4 }} style={{ display: 'flex', width: '350px', height: '240px', overflow: 'scroll' }}>
+                        {Images.map((image, index) => (
+                            <img key={index} style={{ minWidth: '300px', width: '300px', height: '240px' }} src={`${image}`} />
                         ))}
-                    </Input>
-                    <Button className="col-md-2 offset-md-10 mt-5" onClick={submitHandler}>
-                        확인
-                    </Button>
-                </Form>
+                    </Col>
+                    <Form onSubmit={submitHandler} name="myform">
+                        <Label>제목</Label>
+                        <Input onChange={titleChangeHandler} value={Title} name="title" />
+                        <br />
+                        <Label>설명</Label>
+                        <Input type="textarea" onChange={descriptionChangeHandler} value={Description} name="description" />
+                        <br />
+                        <Label>장르</Label>
+                        <Input onChange={continentChangeHandler} value={Continent} name="continent" type="select">
+                            <option value="">장르를 선택해주세요</option>
+                            {Continents.map((item) => (
+                                <option key={item.key} value={item.key}>
+                                    {item.value}
+                                </option>
+                            ))}
+                        </Input>
+                        <Button className="col-md-2 offset-md-10 mt-5" onClick={submitHandler}>
+                            확인
+                        </Button>
+                    </Form>
+                </CardBody>
             </Card>
+        </Col>
     );
 };
 
