d48b12f8e92b24785cb3b25da9cfee71fd0d96f2
k0502s <k0502s@naver.com>
2021-04-21 18:40:09 +0900

    홈 화면 슬라이더 사진 이미지 개별로 디테일 포토 라우터 링크 삽입

--- client/src/components/Main/LandingPage/Section/PhotoImage.js
+++ client/src/components/Main/LandingPage/Section/PhotoImage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import * as S from '../LandingPage.style';
 import { Slide } from 'react-slideshow-image';
-import { Fade } from 'react-slideshow-image';
 import { useSelector } from 'react-redux';
 
 const PhotoImage = () => {
@@ -11,9 +11,21 @@ const PhotoImage = () => {
         duration: 5000,
         transitionDuration: 500,
         infinite: true,
-        prevArrow: <div style={{width: "30px", marginRight: "-30px"}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#fff"><path d="M242 180.6v-138L0 256l242 213.4V331.2h270V180.6z"/></svg></div>,
-        nextArrow: <div style={{width: "30px", marginLeft: "-30px"}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#fff"><path d="M512 256L270 42.6v138.2H0v150.6h270v138z"/></svg></div>
-      };
+        prevArrow: (
+            <div style={{ width: '30px', marginRight: '-30px' }}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#fff">
+                    <path d="M242 180.6v-138L0 256l242 213.4V331.2h270V180.6z" />
+                </svg>
+            </div>
+        ),
+        nextArrow: (
+            <div style={{ width: '30px', marginLeft: '-30px' }}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#fff">
+                    <path d="M512 256L270 42.6v138.2H0v150.6h270v138z" />
+                </svg>
+            </div>
+        ),
+    };
     useEffect(() => {
         console.log([...bestimages]);
         let images = [];
@@ -23,6 +35,7 @@ const PhotoImage = () => {
             images.push({
                 original: `${item.images[0]}`,
                 thumbnail: `${item.images[0]}`,
+                link: `${item._id}`,
             });
         });
         setImages(images);
@@ -33,9 +46,11 @@ const PhotoImage = () => {
             <S.wrap>
                 <Slide easing="ease" {...properties}>
                     {Images.map((images, index) => (
-                        <div className="each-slide" index={index}>
-                            <div style={{ backgroundImage: `url(${images.original})` }}></div>
-                        </div>
+                        <Link to={`photo/${images.link}`}>
+                            <div className="each-slide" index={index}>
+                                <div style={{ backgroundImage: `url(${images.original})` }}></div>
+                            </div>
+                        </Link>
                     ))}
                 </Slide>
             </S.wrap>
