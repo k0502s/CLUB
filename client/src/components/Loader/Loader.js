2ad35b8f6b77d93d38f6c9b7dcc5afe67a252bfb
k0502s <k0502s@naver.com>
2021-04-25 23:14:18 +0900

    로더 CSS 스타일 수정

--- client/src/components/Loader/Loader.js
+++ client/src/components/Loader/Loader.js
@@ -1,17 +1,12 @@
-import React, { Fragment } from 'react';
-import { Row, Spinner } from 'reactstrap';
+import React from 'react';
+import * as S from './Loader.style'
 
 export const Loader = (
-    <Fragment>
-        <Row className="d-flex justify-content-center m-5">
-            <Spinner style={{ width: '2rem', height: '2rem' }} color="primary" />
-            <Spinner style={{ width: '2rem', height: '2rem' }} color="secondary" />
-            <Spinner style={{ width: '2rem', height: '2rem' }} color="success" />
-            <Spinner style={{ width: '2rem', height: '2rem' }} color="danger" />
-            <Spinner style={{ width: '2rem', height: '2rem' }} color="warning" />
-            <Spinner style={{ width: '2rem', height: '2rem' }} color="info" />
-            <Spinner style={{ width: '2rem', height: '2rem' }} color="light" />
-            <Spinner style={{ width: '2rem', height: '2rem' }} color="dark" />
-        </Row>
-    </Fragment>
+    <>
+        <S.LoaderWrap>
+            <S.Loader margin={'8px'} color="danger" />
+            <S.Loader margin={'20px'} color="warning" />
+            <S.Loader margin={'20px'} color="info" />
+        </S.LoaderWrap>
+    </>
 );
