6f41c46790d2d91a02a37d103cd485a563e9c37e
k0502s <k0502s@naver.com>
2021-06-28 19:01:33 +0900

    Router 새로고침시 404 에러 페이지 오류 수정

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,15 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import store, { history } from './store';
 import MyRouter from './components/Myrouter/Router';
+import { HashRouter } from 'react-router-dom';
 
 const App = () => {
     return (
         <Provider store={store}>
             <ConnectedRouter history={history}>
-                <MyRouter />
+                <HashRouter>
+                    <MyRouter />
+                </HashRouter>
             </ConnectedRouter>
         </Provider>
     );
